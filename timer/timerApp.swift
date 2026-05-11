484ab4137b15ac21b6528cf42fc8ca9258f891a1
amay korade <amay@rkscloud.com>
2026-05-12 00:00:36 +0530

    Initial Commit

---
+++ timer/timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  timerApp.swift
+//  timer
+//
+//  Created by Amay Ramaling Korade on 12/05/26.
+//
+
+import SwiftUI
+
+@main
+struct timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
